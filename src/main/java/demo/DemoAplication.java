aa9447f9363ef56c36b1c6e95408f4a7feec75ad
DidacusZ <diegoooooooooooooooo16@gmail.com>
2023-11-15 06:01:58 +0100

    erroor

---
+++ src/main/java/demo/DemoAplication.java
@@ -0,0 +1,15 @@
+package demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing
+public class DemoAplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoAplication.class, args);
+	}
+
+}
